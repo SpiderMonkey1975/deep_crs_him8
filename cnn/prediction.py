6372dce3aa69ea8176ce66861ff0bd3814933693
Mark Cheeseman <mark.cheeseman@pawsey.org.au>
2019-04-18 02:22:02 +0000

    updated prediction.py to use new neural net functions

--- cnn/prediction.py
+++ cnn/prediction.py
@@ -3,17 +3,17 @@ from tensorflow.keras.layers import BatchNormalization, Conv2D, Conv2DTranspose
 from tensorflow.keras.optimizers import Adam
 from datetime import datetime
 import numpy as np
-import argparse
+import argparse, cnn
 
 ##
 ## Look for any user specified commandline arguments
 ##---------------------------------------------------
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', '--arch', type=str, default="pascal", help="set the GPU architecture. Valid values are volta, pascal or kepler")
-parser.add_argument('-b', '--batch_size', type=int, default=32, help="set batch size to the GPU")
+parser.add_argument('-b', '--batch_size', type=int, default=4, help="set batch size to the GPU")
 parser.add_argument('-c', '--channels', type=int, default=3, help="number of channels in input data. Valid values are 3, 4, 5, 10")
 parser.add_argument('-n', '--num_tries', type=int, default=5, help="number of inference tests. Minimum value of 5")
+parser.add_argument('-p', '--padding', type=str, default="same", help="set ipadding type for CNN. Valid values are valid or same")
 args = parser.parse_args()
 
 if args.channels != 3 and args.channels != 4 and args.channels != 5 and args.channels != 10:
@@ -25,70 +25,35 @@ if args.num_tries < 5:
 if args.arch != "kepler" and args.arch != "pascal" and args.arch != "volta":
    args.arch = "pascal"
 
-
-##
-## Form the neural network
-##-------------------------
-
-def GetModel( num_channels ):
-    model = Sequential()
-    model.add(BatchNormalization(axis=3, input_shape=(400, 400, num_channels)))
-    # Size 400x400x3
-    model.add(Conv2D(32, 3, strides=2, activation='relu'))
-    model.add(BatchNormalization(axis=3))
-    # Size 200x200x32
-    model.add(Conv2D(64, 3, strides=2, activation='relu'))
-    model.add(BatchNormalization(axis=3))
-    # Size 100x100x64
-    model.add(Conv2D(128, 3, strides=2, activation='relu'))
-    model.add(BatchNormalization(axis=3))
-    # Size 50x50x128
-    model.add(Conv2D(256, 3, strides=2, activation='relu'))
-    model.add(BatchNormalization(axis=3))
-    # Size 25x25x256
-    model.add(Conv2DTranspose(128, 3, strides=2, activation='relu'))
-    model.add(BatchNormalization(axis=3))
-    # Size 50x50x128
-    model.add(Conv2DTranspose(64, 3, strides=2, activation='relu'))
-    model.add(BatchNormalization(axis=3))
-    # Size 100x100x64
-    model.add(Conv2DTranspose(32, 3, strides=2, activation='relu'))
-    model.add(BatchNormalization(axis=3))
-    # Size 200x200x32
-    model.add(Conv2DTranspose(1, 3, strides=2, activation='relu'))
-    # Size 400x400x1
-
-    model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.0001), metrics=['mae'])
-    return model
-
-
-##
-## Create model
-##--------------
-
-model = GetModel( args.channels )
-
-
-##
-## Load pre-trained model weights
-##--------------------------------
-
-filename = "model_weights/" + args.arch + "/model_weights_cnn_" + str(args.channels) + "channels.h5"
-model.load_weights( filename )
-
+if args.padding != "valid" and args.padding != "same":
+   args.padding = "same"
 
 ##
 ## Read in the input (X,Y) datasets
 ##----------------------------------
-
 x_file = "input/input_" + str(args.channels) + "layer.npy"
 x = np.load( x_file )
 
+##
+## Form the neural network
+##-------------------------
+num_filters = 5
+if args.padding == "valid":
+   num_filters = 3
+
+model = cnn.cnn( args.channels, args.padding, num_filters )
+
+model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.0001), metrics=['mae'])
+
+##
+## Load pre-trained model weights
+##--------------------------------
+filename = "model_weights/" + args.arch + "/weights_" + str(args.channels) + "channels_" + args.padding + "_padding.h5"
+model.load_weights( filename )
 
 ##
 ## Perform inference testing
 ##---------------------------
-
 print(" ")
 print(" ")
 print("=====================================================================================")
