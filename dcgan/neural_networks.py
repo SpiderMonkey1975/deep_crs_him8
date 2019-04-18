74fb2d03ede0863fe87453c966cc6c663b954f4b
Mark Cheeseman <mark.cheeseman@pawsey.org.au>
2019-04-18 07:53:51 +0000

    neural networks for GAN defined

--- dcgan/neural_networks.py
+++ dcgan/neural_networks.py
@@ -1,6 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.activations import relu, sigmoid
-from tensorflow.keras.layers import Dense, DropOut, BatchNormalization, Conv2D, Conv2DTranspose, Flatten, UpSampling2D
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, Conv2D, Conv2DTranspose, Flatten, UpSampling2D, Reshape
 
 ##
 ## discriminator 
@@ -12,25 +11,20 @@ from tensorflow.keras.layers import Dense, DropOut, BatchNormalization, Conv2D, 
 def discriminator():
     model = Sequential()
 
-    model.add( Conv2D( 32, 5, strides=2, activation='none', input_shape=(400,400,1), padding='same' ) )
-    model.add( relu(alpha=0.2) )
+    model.add( Conv2D( 32, 5, strides=2, activation='relu', input_shape=(400,400,1), padding='same' ) )
     model.add( Dropout(rate=0.4) )
 
-    model.add( Conv2D( 64, 3, strides=2, activation='none', padding='same' ) )
-    model.add( relu(alpha=0.2) )
+    model.add( Conv2D( 64, 3, strides=2, activation='relu', padding='same' ) )
     model.add( Dropout(rate=0.4) )
 
-    model.add( Conv2D( 128, 3, strides=2, activation='none', padding='same' ) )
-    model.add( relu(alpha=0.2) )
+    model.add( Conv2D( 128, 3, strides=2, activation='relu', padding='same' ) )
     model.add( Dropout(rate=0.4) )
 
-    model.add( Conv2D( 256, 3, strides=2, activation='none', padding='same' ) )
-    model.add( relu(alpha=0.2) )
+    model.add( Conv2D( 256, 3, strides=2, activation='relu', padding='same' ) )
     model.add( Dropout(rate=0.4) )
 
     model.add( Flatten() )
-    model.add( Dense(1) )
-    model.add( sigmoid() )
+    model.add( Dense(1, activation='sigmoid') )
     return model
 
 ##
@@ -42,10 +36,10 @@ def discriminator():
 def generator(num_channels):
     model = Sequential()
 
-    model.add( Flatten(input_shape=(400,400,num_channels)) )
-    model.add( Dense(400*400*num_channels,activation='relu') )
-    model.add( BatchNormalization(momentum=0.9) )
-    model.add( Reshape((50,50,16*num_channels)) )
+#    model.add( Flatten(input_shape=(400,400,num_channels)) )
+#    model.add( Dense(400*400*num_channels,activation='relu') )
+#    model.add( BatchNormalization(momentum=0.9) )
+    model.add( Reshape((50,50,64*num_channels), input_shape=(400,400,num_channels)) )
     model.add( Dropout(rate=0.4) )
 
     model.add( UpSampling2D() )
--- dcgan/train.py
+++ dcgan/train.py
@@ -1,117 +1,80 @@
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, History
-from datetime import datetime
+from tensorflow.keras.optimizers import RMSprop 
 import numpy as np
-import argparse, cnn
+import argparse, neural_networks, sys, random
 
-##
-## Look for any user specified commandline arguments
-##
+##----------------
+## Initial Setup
+##----------------
 
+# parse commandline arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-a', '--arch', type=str, default="volta", help="set the GPU architecture. Valid values are volta, pascal or kepler")
-parser.add_argument('-p', '--padding', type=str, default="valid", help="set ipadding type for CNN. Valid values are valid or same")
-parser.add_argument('-b', '--batch_size', type=int, default=32, help="set batch size to the GPU")
+parser.add_argument('-b', '--batch_size', type=int, default=8, help="set batch size to the GPU")
 parser.add_argument('-c', '--channels', type=int, default=3, help="number of channels in input data. Valid values are 3, 4, 5, 10")
-parser.add_argument('-n', '--num_trials', type=int, default=5, help="number of trials for training. Valid value is between 1 and 5")
 args = parser.parse_args()
 
 if args.channels != 3 and args.channels != 4 and args.channels != 5 and args.channels != 10:
    args.channels = 3
 
-if args.arch != "kepler" and args.arch != "pascal" and args.arch != "volta":
-   args.arch = "volta"
-
-if args.num_trials<1:
-   args.num_trials = 1
-
-if args.num_trials>5:
-   args.num_trials = 5
-
-if args.padding != "valid" and args.padding != "same":
-   args.padding = "same"
-
-##
-## Read in the input (X,Y) datasets
-##----------------------------------
-
-input_dir = "input/"
+# read in feature and label datasets 
+input_dir = "../cnn/input/"
 
 y_file = input_dir + "crs.npy"
 x_file = input_dir + "input_" + str(args.channels) + "layer.npy"
 
-x = np.load( x_file )
-y = np.load( y_file )[:,:,:,None]
+x = np.load( x_file )[:2473,:,:,:]
+y_true = np.load( y_file )[:2473,:,:,None]
 
-##
-## Form the neural network
-##-------------------------
 
-num_filters = 5
-if args.padding == "valid":
-   num_filters = 3
-   y = y[:,:399,:399,:]
+##--------------------------------------------
+## Generate the initial "fake" label dataset
+##--------------------------------------------
 
-model = cnn.cnn( args.channels, args.padding, num_filters )
+# create the generator model
+generator = neural_networks.generator( args.channels )
+generator.compile( loss='binary_crossentropy', 
+                   optimizer=RMSprop(lr=0.0002,decay=3e-8), 
+                   metrics=['accuracy'] )
 
-model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.0001), metrics=['mae'])
+# train it for 2 epochs
+hist = generator.fit( x, y_true, batch_size=args.batch_size, verbose=0, epochs=2, validation_split=.25, shuffle=True )
 
-##
-## Set up the training of the model
-##----------------------------------
+# use partially trained generator to construct fake input data
+y_fake = generator.predict( x, batch_size=args.batch_size, verbose=0 )
 
-filename = "model_weights/" + args.arch + "/weights_" + str(args.channels) + "channels_" + args.padding + "_padding.h5"
-checkpoint = ModelCheckpoint( filename, 
-                              monitor='val_loss', 
-                              save_best_only=True, 
-                              mode='min' )
+# one-hot encoding. 1 denotes a true image, 0 denotes a fake image
+true_ids = random.sample( range(y_true.shape[0]), 1237 )
 
-earlystop = EarlyStopping( min_delta=0.0005,
-                           patience=10,
-                           mode='min' )
+one_hot_encoding = np.zeros((y_true.shape[0],1), dtype=int)
 
-history = History()
+feature_data = y_fake
+for n in true_ids:
+    one_hot_encoding[n] = 1
+    feature_data[n,:,:,:] = y_true[n,:,:,:]
+ 
+##--------------------------
+## Train the discriminator 
+##--------------------------
 
-my_callbacks = [checkpoint, earlystop, history]
+discriminator = neural_networks.discriminator()
+discriminator.compile( loss='binary_crossentropy', 
+                       optimizer=RMSprop(lr=0.0001,decay=6e-8), 
+                       metrics=['accuracy'] )
 
-print(" ")
-print(" ")
-print("=====================================================================================")
-print("                          Rainfall Regression Network")
-print("=====================================================================================")
-print(" ")
-print("   %s GPU architecture used" % args.arch)
-print("   %2d channels of satellite data used" % args.channels)
-print("   batch size of %3d images used" % args.batch_size)
+hist = discriminator.fit( feature_data, one_hot_encoding, 
+                          batch_size=args.batch_size, 
+                          epochs=10, 
+                          validation_split=.25, 
+                          shuffle=True )
 
-for n in range(args.num_trials):
+##----------------------
+## Train the generator 
+##----------------------
 
-##
-## Perform model training
-##------------------------
+discriminator_guesses = discriminator.predict( feature_data, batch_size=args.batch_size, verbose=0 )
 
-   t1 = datetime.now()
-   hist = model.fit( x, y, 
-                     batch_size=args.batch_size,
-                     epochs=100, 
-                     verbose=0, 
-                     validation_split=.25,
-                     callbacks=my_callbacks, 
-                     shuffle=True )
-   training_time = (datetime.now()-t1 ).total_seconds()
+hist = generator.fit( x, discriminator_guesses, 
+                      batch_size=args.batch_size,
+                      epochs=10,
+                      validation_split=.25,
+                      shuffle=True )
 
-   val_loss = hist.history['val_loss']
-   min_val_loss = 10000000.0
-   ind = -1
-   for n in range(len(val_loss)):
-       if val_loss[n] < min_val_loss:
-          min_val_loss = val_loss[n]
-          ind = n
-
-   print(" ")
-   print("   TRAINING OUTPUT")
-   print("       minimum val_loss was %8.6f" % min_val_loss)
-   print("       minimum val_loss occurred at epoch %2d" % ind)
-   print("       training lasted for %7.1f seconds" % training_time)
-
-print(" ")
