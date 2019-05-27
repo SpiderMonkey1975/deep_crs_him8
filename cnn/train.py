db67f88d52706083513c34ffe6c272da8a854261
Mark Cheeseman <mark.cheeseman@pawsey.org.au>
2019-05-27 03:56:00 +0000

    updated regression training code

--- cnn/train.py
+++ cnn/train.py
@@ -1,6 +1,8 @@
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, History
 from datetime import datetime
+from my_utils import plot_images
+
 import numpy as np
 import argparse, cnn
 
@@ -9,39 +11,29 @@ import argparse, cnn
 ##
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--padding', type=str, default="valid", help="set ipadding type for CNN. Valid values are valid or same")
+parser.add_argument('-p', '--padding', type=str, default="same", help="set padding type for CNN. Valid values are valid or same")
 parser.add_argument('-b', '--batch_size', type=int, default=32, help="set batch size to the GPU")
-parser.add_argument('-c', '--channels', type=int, default=3, help="number of channels in input data. Valid values are 3, 4, 5, 10")
-parser.add_argument('-n', '--num_trials', type=int, default=5, help="number of trials for training. Valid value is between 1 and 5")
+parser.add_argument('-c', '--channels', type=int, default=3, help="number of channels in input data. Valid values are 3 and 10")
 args = parser.parse_args()
 
-if args.channels != 3 and args.channels != 4 and args.channels != 5 and args.channels != 10:
+if args.channels != 3 and args.channels != 10:
    args.channels = 3
-
-if args.num_trials<1:
-   args.num_trials = 1
-
-if args.num_trials>5:
-   args.num_trials = 5
 
 if args.padding != "valid" and args.padding != "same":
    args.padding = "same"
 
 ##
 ## Read in the input (X,Y) datasets
-##----------------------------------
+##
 
-input_dir = "input/"
+filename= "../input/input_" + str(args.channels) + "layer_train.npy"
+x = np.load( filename )
 
-y_file = input_dir + "crs.npy"
-x_file = input_dir + "input_" + str(args.channels) + "layer.npy"
-
-x = np.load( x_file )
-y = np.load( y_file )[:,:,:,None]
+y = np.load( "../input/crs_train.npy" )[:,:,:,None]
 
 ##
 ## Form the neural network
-##-------------------------
+##
 
 num_filters = 5
 if args.padding == "valid":
@@ -54,9 +46,9 @@ model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.0001), metrics=['ma
 
 ##
 ## Set up the training of the model
-##----------------------------------
+##
 
-filename = "model_weights/weights_" + str(args.channels) + "channels_" + args.padding + "_padding.h5"
+filename = "weights_" + str(args.channels) + "channels_" + args.padding + "_padding.h5"
 checkpoint = ModelCheckpoint( filename, 
                               monitor='val_loss', 
                               save_best_only=True, 
@@ -70,43 +62,70 @@ history = History()
 
 my_callbacks = [checkpoint, earlystop, history]
 
+##
+## Perform model training
+##
+
+t1 = datetime.now()
+hist = model.fit( x, y, 
+                  batch_size=args.batch_size,
+                  epochs=100, 
+                  verbose=0, 
+                  validation_split=.25,
+                  callbacks=my_callbacks, 
+                  shuffle=True )
+training_time = (datetime.now()-t1 ).total_seconds()
+
+val_loss = hist.history['val_loss']
+min_val_loss = 10000000.0
+ind = -1
+for n in range(len(val_loss)):
+       if val_loss[n] < min_val_loss:
+          min_val_loss = val_loss[n]
+          ind = n
+
 print(" ")
 print(" ")
 print("=====================================================================================")
 print("                          Rainfall Regression Network")
 print("=====================================================================================")
 print(" ")
-print("   %2d channels of satellite data used" % args.channels)
+if args.channels == 10:
+   print("   %2d channels of satellite data used" % args.channels)
+else:
+   print("   %1d channels of satellite data used" % args.channels)
 print("   batch size of %3d images used" % args.batch_size)
-
-for n in range(args.num_trials):
+print(" ")
+print("   TRAINING OUTPUT")
+print("       minimum val_loss was %8.6f" % min_val_loss)
+print("       minimum val_loss occurred at epoch %2d" % ind)
+print("       training lasted for %7.1f seconds" % training_time)
+print(" ")
 
 ##
-## Perform model training
-##------------------------
+## Perform model evaluation
+##
 
-   t1 = datetime.now()
-   hist = model.fit( x, y, 
-                     batch_size=args.batch_size,
-                     epochs=100, 
-                     verbose=0, 
-                     validation_split=.25,
-                     callbacks=my_callbacks, 
-                     shuffle=True )
-   training_time = (datetime.now()-t1 ).total_seconds()
+filename = "../input/input_" + str(args.channels) + "layer_test.npy"
+reflectance_data = np.load( filename )
+real_images = np.load( "../input/crs_test.npy" )[:,:,:,None]
 
-   val_loss = hist.history['val_loss']
-   min_val_loss = 10000000.0
-   ind = -1
-   for n in range(len(val_loss)):
-       if val_loss[n] < min_val_loss:
-          min_val_loss = val_loss[n]
-          ind = n
+print("   EVALUATION OUTPUT")
 
-   print(" ")
-   print("   TRAINING OUTPUT")
-   print("       minimum val_loss was %8.6f" % min_val_loss)
-   print("       minimum val_loss occurred at epoch %2d" % ind)
-   print("       training lasted for %7.1f seconds" % training_time)
+t1 = datetime.now()
+score = model.evaluate( reflectance_data, real_images, batch_size=args.batch_size, verbose=0 )
+inference_time = (datetime.now()-t1).total_seconds()
+print("       inference took %5.1f seconds" % inference_time )
+print("       mean square error was %12.10f" % score[1] )
 
 print(" ")
+print(" ")
+
+##
+## Output some visual comparisons
+##
+
+idx = np.random.choice( real_images.shape[0], 5 ) 
+real_images = real_images[ idx,:,: ]
+fake_images = model.predict( reflectance_data[ idx,:,:,: ] )
+plot_images( args.channels, real_images, fake_images )
